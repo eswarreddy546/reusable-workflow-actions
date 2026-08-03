fee027333e57407c3f9dbe39a578b3a24b72db90
Asri <asri>
2026-08-03 21:49:01 +0530

    Add terraform-actions as normal folder

---
+++ terraform-actions/variables.tf
@@ -0,0 +1,19 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_name" {
+  type        = string
+  default     = "eswar.fun"
+  description = "description"
+}
+
+variable "zone_id" {
+  type        = string
+  default     = "Z0883794XJ2EA3764V8N"
+  description = "description"
+}
